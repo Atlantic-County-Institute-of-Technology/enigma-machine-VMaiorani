38187dc7f20fe3aabdb802bf79557bd47f1f9d3c
Vince <maio9545@student.acitech.org>
2025-12-16 10:52:37 -0500

    made the cipher looks a lot better but still dosen't fully work as intented

--- main.py
+++ main.py
@@ -3,27 +3,33 @@ def Make_Key(msg,KEY):
         return KEY
     else:
         for i in range(len(msg) - len(KEY)):
-            KEY = KEY + KEY[i % len(KEY)]
+            KEY = KEY[i % len(KEY)]
             print(KEY)
         return"".join(KEY)
 
 def Vigenere_Cipher(msg, KEY):
     encrypt = []
-    KEY = Make_Key(msg, KEY)
+    m = 0
     for i in range(len(msg)):
         char = msg[i]
-        if char.isupper():
-            encrypted_char = chr((ord(char) + ord(KEY[i]) - 2 * ord('A')) % 26 + ord('A'))
-            print(encrypted_char)
-        elif char.islower():
-            encrypted_char = chr((ord(char) + ord(KEY[i]) - 2 * ord('A')) % 26 + ord('a'))
-            print(encrypted_char)
-        #     awedh
+        print(char, KEY[m % len(KEY)])
+        if char.isalpha():
+            if char.isupper():
+                encode = (((ord(char) - ord('A')) - (ord(KEY[m % len(KEY)].upper()) - ord('A'))) % 26)
+                print(encode)
+                encode_text = chr(encode + ord('A'))
+            elif char.islower():
+                encode = (((ord(char) - ord('a')) - (ord(KEY[m % len(KEY)].lower()) - ord('a'))) % 26)
+                encode_text = chr(encode + ord('a'))
+                print(encode)
+            m += 1
+            # elif char.islower():
         else:
-            encrypted_char = char
-        encrypt.append(encrypted_char)
+            encode_text = char
+        encrypt.append(encode_text)
     return "".join(encrypt)
 
+# odlko
 
 
 
@@ -39,3 +45,23 @@ KEY = input(str("please input a word or a phrase of letter for your key : "))
 
 Excepted_Text = Vigenere_Cipher(msg, KEY)
 print(Excepted_Text)
+
+# KEY = Make_Key(msg, KEY)
+# for i in range(len(msg)):
+#     char = msg[i]
+#     if char.isupper():
+#         encrypted_char = chr(((ord(char) - ord('A')) - ord(KEY[i % len(KEY)]) - ord('A')) % 26)
+#         print(encrypted_char)
+#     elif char.islower():
+#         encrypted_char = chr((ord(char) - ord(KEY[i]) - ord('A')) % 26)
+#         # if char.isupper():
+#         #     encrypted_char = chr((ord(char) - ord(KEY[i]) - 2 * ord('A')) % 26 + ord('A'))
+#         #     print(encrypted_char)
+#         # elif char.islower():
+#         #     encrypted_char = chr((ord(char) - ord(KEY[i]) - 2 * ord('A')) % 26 + ord('a'))
+#         print(encrypted_char)
+#     #     awedh
+#     else:
+#         encrypted_char = char
+#     encrypt.append(encrypted_char)
+# return "".join(encrypt)
