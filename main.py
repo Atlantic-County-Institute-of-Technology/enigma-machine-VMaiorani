1e5da443468067e5ad72d29c24bc830214dc853c
Vince <maio9545@student.acitech.org>
2025-12-18 10:48:38 -0500

    Starting working on the decoder and the menu

--- main.py
+++ main.py
@@ -1,64 +1,99 @@
 
+
 def Make_Key(msg,KEY):
     if len(msg) == len(KEY):
         return KEY
     else:
         for i in range(len(msg) - len(KEY)):
             KEY = KEY[i % len(KEY)]
-            # print(KEY)
+            i += 1
+            print(KEY)
         return"".join(KEY)
 
-def Vigenere_Cipher(msg, KEY):
+def Vigenere_Cipher(msg,KEY):
     Make_Key(msg, KEY)
     encrypt = []
     m = 0
     for i in range(len(msg)):
         char = msg[i]
-        # print(char, KEY[m % len(KEY)])
+        print(char, KEY[m % len(KEY)])
         if char.isalpha():
             if char.isupper():
                 encode = (((ord(char) - ord('A')) + (ord(KEY[m % len(KEY)].upper()) - ord('A'))) % 26)
                 encode_text = chr(encode + ord('A'))
+                encrypt.append(encode_text)
             elif char.islower():
                 encode = (((ord(char) - ord('a')) + (ord(KEY[m % len(KEY)].lower()) - ord('a'))) % 26)
                 encode_text = chr(encode + ord('a'))
+                encrypt.append(encode_text)
             # print(encode)
             # print(char)
             m += 1
         else:
             encode_text = char
-        encrypt.append(encode_text)
+            encrypt.append(encode_text)
     return "".join(encrypt)
 
-msg = input(str("please input a word, a phrase, mabye even a sentence : "))
-KEY = input(str("please input a word or a phrase of letter for your key : "))
-
-Excepted_Text = Vigenere_Cipher(msg, KEY)
-print("the encryted vesion of " +str(msg) + " is " + Excepted_Text)
-
-
-print("---------------Menu-------------------\n"
-      "1. Would you like to move this +\n"
-      "2. change the \n"
-      "3. start the Cipher")
-# menu = int(input("What options would you like to choose"))
-# def main():
-#     while True:
+# def Dedcode_Cipher(Excepted_Text, KEY):
+#     Make_Key(msg, KEY)
+#     decode = []
+#     m = 0
+#     for i in range(len(Excepted_Text)):
+#         char = Excepted_Text[i]
+#         print(char, KEY[m % len(KEY)])
+#         if char.isalpha():
+#             if char.isupper():
+#                 decode = (((ord(char) + ord('A')) + (ord(KEY[m % len(KEY)].upper()) - ord('A'))) % 26)
+#                 decode_text = chr(decode + ord('A'))
+#                 print(decode_text)
 #
-#         if menu == 1:
-#             KEY = input(str("please input a word or a phrase of letter for your key : "))
+#             elif char.islower():
+#                 decode = (((ord(char) + ord('a')) + (ord(KEY[m % len(KEY)].lower()) - ord('a'))) % 26)
+#                 decode_text = chr(decode + ord('a'))
+#                 print(decode_text)
 #
-#         elif menu == 2:
-#             msg = input(str("please input a word, a phrase, or sentence However you will need to avoid using spaces: "))
-#
-#         elif menu == 3:
-#             Excepted_Text = Vigenere_Cipher(msg, KEY)
-#             print(Excepted_Text)
+#             # print(encode)
+#             # print(char)
+#             m += 1
 #         else:
-#             print("please try selecting an available option")
+#             decode_text = char
+#             print(decode_text)
+#         # decode.append(decode_text)
+#     return "".join(decode)
+
+# msg = input(str("please input a word, a phrase, mabye even a sentence : "))
+# KEY = input(str("please input a word or a phrase of letter for your key : "))
 #
-# if __name__ == "__main__":
-#     main()
+# Excepted_Text = Vigenere_Cipher(msg, KEY)
+# print("the encryted vesion of " +str(msg) + " is " + Excepted_Text)
+
+# Decrypted_Text = Dedcode_Cipher(Excepted_Text, KEY)
+# print(Decrypted_Text)
+def main():
+
+    while True:
+        print("---------------Menu-------------------\n"
+              "1. Make a cipher \n"
+              "2. overwrite current file \n"
+              "3. decode file content\n"
+              "--------------------------------------")
+        menu = int(input("What options would you like to choose: "))
+
+        if menu == 1:
+            msg = input(str("please input a word, a phrase, or sentence However you will need to avoid using spaces: "))
+            KEY = input(str("please input a word or a phrase of letter for your key : "))
+            Excepted_Text = Vigenere_Cipher(msg, KEY)
+            Vigenere_Cipher(msg, KEY)
+            print("the encryted vesion of " + str(msg) + " is " + Excepted_Text)
+        elif menu == 2:
+            print("In working progress")
+        elif menu == 3:
+            print("In working progress")
+        else:
+            print("please try selecting an available option")
+
+if __name__ == "__main__":
+    main()
 
 # ---To Do list----
 # > Make cypher - COMPLETED -
